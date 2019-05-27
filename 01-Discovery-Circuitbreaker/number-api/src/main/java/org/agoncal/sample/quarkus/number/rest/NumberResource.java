f1f6910be0522bddf0dae514bde38c0f21364091
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-05-27 17:11:06 +0200

    Adding Service Discovery and Curcuit Breaker first code

---
+++ 01-Discovery-Circuitbreaker/number-api/src/main/java/org/agoncal/sample/quarkus/number/rest/NumberResource.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.agoncal.sample.quarkus.number.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@ApplicationScoped
+@Path("numbers")
+@Produces(MediaType.TEXT_PLAIN)
+public class NumberResource {
+
+    private final Logger log = LoggerFactory.getLogger(NumberResource.class);
+
+    @GET
+    @Path("book")
+    public Response generateBookNumber() {
+        log.info("Generating a book number");
+        return Response.ok("BK-" + Math.random()).build();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# agoncal-sample-quarkus
+# Samples - JAX-RS
+
+## Purpose of this sample
+
+Several samples about JAX-RS : 
+
+* 01-Testing : Simple integration test
+
+# Licensing
+
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.
+
+<div class="footer">
+    <span class="footerTitle"><span class="uc">a</span>ntonio <span class="uc">g</span>oncalves</span>
+</div>

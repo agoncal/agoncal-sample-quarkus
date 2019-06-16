0516f1953abb4ad9400ba12f701e9b48b6e412b7
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-06-16 20:22:45 +0200

    adding listener

---
+++ 02-ConfigurationProfile/src/main/java/org/agoncal/sample/quarkus/book/ApplicationLifeCycle.java
@@ -0,0 +1,23 @@
+package org.agoncal.sample.quarkus.book;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+
+@ApplicationScoped
+public class ApplicationLifeCycle {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("ApplicationLifeCycle");
+
+    void onStart(@Observes StartupEvent ev) {
+        LOGGER.info("The application is starting...");
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {
+        LOGGER.info("The application is stopping...");
+    }
+}

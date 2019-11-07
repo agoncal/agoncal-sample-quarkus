88375238aa3d7e783285c4066417f83e572e75f3
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-11-07 12:40:32 +0100

    Fixing Tests

--- 01-Configuration/src/test/java/org/agoncal/sample/quarkus/book/rest/BookResourceTest.java
+++ 01-Configuration/src/test/java/org/agoncal/sample/quarkus/book/rest/BookResourceTest.java
@@ -19,28 +19,30 @@ package org.agoncal.sample.quarkus.book.rest;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.agoncal.sample.quarkus.book.domain.Book;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import javax.json.bind.JsonbBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import static io.restassured.RestAssured.given;
-import static javax.ws.rs.client.Entity.entity;
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
+import static javax.ws.rs.core.HttpHeaders.ACCEPT;
+import static javax.ws.rs.core.HttpHeaders.CONTENT_TYPE;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.Response.Status.CREATED;
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 import static javax.ws.rs.core.Response.Status.NO_CONTENT;
 import static javax.ws.rs.core.Response.Status.OK;
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BookResourceTest {
 
-    private WebTarget webTarget;
+    private static String bookId;
 
     @Test
     public void shouldNotFindAnythingById() {
@@ -51,60 +53,65 @@ public class BookResourceTest {
     }
 
     @Test
-    public void shouldFindAll() {
+    @Order(1)
+    public void shouldFindAllBeforeCreate() {
         given()
             .when().get("/api/books")
             .then()
             .statusCode(OK.getStatusCode())
-            .body("$", hasSize(1));
+            .body("size()", is(1));
     }
 
     @Test
+    @Order(2)
     public void shouldCreate() {
         final Book book = new Book("Joshua Bloch", "Effective Java (2nd Edition)", 2001, "Tech", " 978-0-3213-5668-0");
         String bookJson = JsonbBuilder.create().toJson(book);
 
-        given().when().get("/api/books").then().body("$", hasSize(1));
-
-        given()
+        String location = given()
             .body(bookJson)
-            .contentType(MediaType.APPLICATION_JSON)
+            .header(CONTENT_TYPE, APPLICATION_JSON)
+            .header(ACCEPT, APPLICATION_JSON)
             .when()
             .post("/api/books")
             .then()
-            .statusCode(CREATED.getStatusCode());
+            .statusCode(CREATED.getStatusCode())
+            .extract().header("Location");
+        assertTrue(location.contains("/api/books"));
 
-        given().when().get("/api/books").then().body("$", hasSize(2));
+        // Stores the id
+        String[] segments = location.split("/");
+        bookId = segments[segments.length - 1];
+        assertNotNull(bookId);
     }
 
     @Test
-    @Disabled
-    public void update() {
-        final Book book = new Book("Joshua Bloch", "Effective Java (3rd Edition)", 2018, "Tech", " 978-0-1346-8599-1");
-        final Response response = webTarget.path("{id}")
-            .resolveTemplate("id", 1)
-            .request()
-            .put(entity(book, APPLICATION_JSON_TYPE));
-
-        assertEquals(OK.getStatusCode(), response.getStatus());
-    }
-
-    @Test
-    @Disabled
-    public void delete() {
-        final Response response = webTarget.path("{id}")
-            .resolveTemplate("id", 1)
-            .request()
-            .delete();
-
-        assertEquals(NO_CONTENT.getStatusCode(), response.getStatus());
-    }
-
-    @Test
-    public void shouldNotDeleteAnythingById() {
+    @Order(3)
+    public void shouldFindAllAfterCreate() {
         given()
-            .when().get("/api/books/9999999")
+            .when().get("/api/books")
             .then()
-            .statusCode(NOT_FOUND.getStatusCode());
+            .statusCode(OK.getStatusCode())
+            .body("size()", is(2));
+    }
+
+    @Test
+    @Order(4)
+    public void delete() {
+        given()
+            .pathParam("id", bookId)
+            .when().delete("/api/books/{id}")
+            .then()
+            .statusCode(NO_CONTENT.getStatusCode());
+    }
+
+    @Test
+    @Order(5)
+    public void shouldFindAllAfterDelete() {
+        given()
+            .when().get("/api/books")
+            .then()
+            .statusCode(OK.getStatusCode())
+            .body("size()", is(1));
     }
 }
